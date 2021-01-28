d590b6abc463989acf709ccf202571415c30039d
BuildToolsUnity <imilly556553@hotmail.com>
2021-01-29 01:57:35 +0700

    Add tween on Search Screen

---
+++ Assets/SearchTween.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SearchTween : MonoBehaviour
+{
+    [Header("Text")]
+    public GameObject textTween;
+    public LeanTweenType typeTweenText;
+    public float SpeedRoate;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        this.transform.localScale = new Vector3(0, 0, 0);
+
+    }
+    private void OnEnable()
+    {
+        LeanTween.scale(textTween, new Vector3(1, 1, 1), SpeedRoate).setEase(typeTweenText);
+    }
+}
